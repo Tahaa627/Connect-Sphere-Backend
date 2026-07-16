75a2f1a85bda609f2e8e3ccb2c11c12ca13237d9
Muhammad Taha <tahabutt627@gmail.com>
2026-07-16 22:52:14 +0500

    User Profile Login Logic:

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from apps.accounts.serializers import UserSerializer
+
+
+class MyProfileView(APIView):
+    """
+    Returns the currently logged-in user's profile.
+    """
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+
+        return Response(serializer.data)
--- config/urls.py
+++ config/urls.py
@@ -19,9 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-    path(
-        "api/auth/",
-        include("apps.accounts.urls"),
-    ),
+    path("api/auth/",include("apps.accounts.urls"),),
+    path("api/profiles/", include("apps.profiles.urls")),
 ]
