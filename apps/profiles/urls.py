a1eaec15384a893b24a25a24922017f347b1f013
Muhammad Taha <tahabutt627@gmail.com>
2026-07-17 20:33:41 +0500

    Added Avatar And CoverView Urls

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import MyProfileView, UpdateProfileView
+from .views import MyProfileView, UpdateProfileView, UploadAvatarView, UploadCoverView
 
 urlpatterns = [
     path("me/", MyProfileView.as_view()),
     path("me/update/", UpdateProfileView.as_view()),
+    path("me/avatar/",UploadAvatarView.as_view(),),
+    path("me/cover/",UploadCoverView.as_view(),),
 ]
