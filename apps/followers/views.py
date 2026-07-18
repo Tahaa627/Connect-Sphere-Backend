80b5a2eb1f5f91ebcc4cb67bbf747e95a4becff7
Muhammad Taha <tahabutt627@gmail.com>
2026-07-18 23:58:53 +0500

    Follower Logic IMplemented

--- apps/followers/views.py
+++ apps/followers/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import FollowUserSerializer
+from .services import follow_user
+
+
+class FollowUserView(APIView):
+    """
+    Follow another user.
+    """
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, username):
+
+        result = follow_user(
+            follower=request.user,
+            username=username,
+        )
+
+        if not result["success"]:
+            return Response(
+                {
+                    "message": result["message"]
+                },
+                status=result["status"],
+            )
+
+        serializer = FollowUserSerializer(result["follow"])
+
+        return Response(
+            {
+                "message": "User followed successfully.",
+                "data": serializer.data,
+            },
+            status=status.HTTP_201_CREATED,
+        )
