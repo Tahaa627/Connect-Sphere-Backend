d7304aa00d5af8afed6d97882b9c73aa84b17067
Muhammad Taha <tahabutt627@gmail.com>
2026-07-19 19:58:27 +0500

    Unfollow Service Implemented

--- apps/followers/services.py
+++ apps/followers/services.py
@@ -55,3 +55,39 @@ def follow_user(follower, username):
         "follow": follow,
         "status": 201,
     }
+
+def unfollow_user(follower, username):
+    """
+    Remove a follow relationship.
+    """
+
+    try:
+        following = User.objects.get(username=username)
+
+    except User.DoesNotExist:
+        return {
+            "success": False,
+            "message": "User does not exist.",
+            "status": 404,
+        }
+
+    try:
+        follow = Follow.objects.get(
+            follower=follower,
+            following=following,
+        )
+
+    except Follow.DoesNotExist:
+        return {
+            "success": False,
+            "message": "You are not following this user.",
+            "status": 400,
+        }
+
+    follow.delete()
+
+    return {
+        "success": True,
+        "message": f"You have unfollowed {following.username}.",
+        "status": 200,
+    }
