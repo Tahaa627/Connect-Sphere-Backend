59742c86f43bf225b9c580786cd1b2a9e3042b99
Muhammad Taha <tahabutt627@gmail.com>
2026-07-21 16:28:54 +0500

    Complete Setup Posts App

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Post,
+    PostImage,
+    Hashtag,
+    Mention,
+)
+
+
+class PostImageInline(admin.TabularInline):
+    model = PostImage
+    extra = 1
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "author",
+        "visibility",
+        "created_at",
+    )
+
+    list_filter = (
+        "visibility",
+        "created_at",
+    )
+
+    search_fields = (
+        "author__username",
+        "content",
+    )
+
+    inlines = [
+        PostImageInline,
+    ]
+
+
+admin.site.register(Hashtag)
+admin.site.register(Mention)
--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'apps.accounts',
     "apps.profiles",
     "apps.followers",
+    "apps.posts",
     'rest_framework_simplejwt.token_blacklist',
      
 ]
