10148d31544d105a34db0736accf23f24303a51b
Muhammad Taha <tahabutt627@gmail.com>
2026-07-21 14:45:03 +0500

    Image Model For Post Model

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -45,3 +45,22 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.author.username} - {self.created_at}"
+    
+class PostImage(models.Model):
+
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="images",
+    )
+
+    image = models.ImageField(
+        upload_to="posts/images/",
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return f"Image {self.id}"
