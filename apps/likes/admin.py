d43016750dfb5ab3ee05db0c9861f61c4eae5121
Muhammad Taha <tahabutt627@gmail.com>
2026-08-07 19:16:47 +0500

    Added Like App Registration In Admin Feature

--- apps/likes/admin.py
+++ apps/likes/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Like
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "user",
+        "post",
+        "created_at",
+    )
+
+    list_filter = (
+        "created_at",
+    )
+
+    search_fields = (
+        "user__username",
+        "post__id",
+    )
