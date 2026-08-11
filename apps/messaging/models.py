034f08afcbe8faaf2b642b8cff3c4d6eb9d4399d
Muhammad Taha <tahabutt627@gmail.com>
2026-08-11 22:05:09 +0500

    Added Conversation Model Feature

--- apps/messaging/models.py
+++ apps/messaging/models.py
@@ -1,3 +1,29 @@
+# Create your models here.
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Conversation(models.Model):
+    """
+    Represents a chat between two or more users.
+    """
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    participants = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        through="ConversationParticipant",
+        related_name="conversations",
+    )
+
+    class Meta:
+        ordering = ["-updated_at"]
+
+    def __str__(self):
+        return f"Conversation {self.id}"
